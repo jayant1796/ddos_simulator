d7b7c35a230cc63d6f2d7534d6dfbc49e01296a4
Jayant Kumar <118969771+jayant1796@users.noreply.github.com>
2024-10-10 01:55:08 +0530

    v2.0

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import threading
 
 app = Flask(__name__)
 
-
 attack_data = []
 attack_active = False
 
@@ -51,4 +50,4 @@ def get_attack_data():
     return jsonify(attack_data), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)  # Set the desired port here
